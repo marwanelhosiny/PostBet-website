bbc2c2f85d389adbe4db2a3b88cb4b5e7b47341a
jemy <mohamed555asdfgh@gmail.com>
2024-10-31 22:05:39 +0300

    Updates

--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./support.scss";
 import { t } from "i18next";
-import { useForm, ValidationError } from "@formspree/react";
+import { useForm } from "@formspree/react";
 import { useToast } from "@chakra-ui/react";
 
 function Support() {
@@ -12,7 +12,12 @@ function Support() {
     subject: "",
     message: "",
   });
-  const [responseData, setResponseData] = useState();
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    subject: false,
+    message: false,
+  });
   const toast = useToast();
   const handleChange = (e) => {
     const { id, value } = e.target;
@@ -24,27 +29,33 @@ function Support() {
     });
   };
   const handleCheckBeforeSubmit = async (e) => {
+    let isErrorGlobal = false;
     e.preventDefault();
-    const { name, email, subject, phone, message } = data;
-    if (!name || !email || !subject || !phone || !message) {
-      return toast({
-        isClosable: true,
-        status: "info",
-        duration: 4000,
-        position: "top",
-        title: t("Fill All Fields"),
-      });
+    for (const key in data) {
+      if (data[key] == "" || data[key] == " ") {
+        setErrors((prev) => {
+          return {
+            ...prev,
+            [key]: true,
+          };
+        });
+        if (!isErrorGlobal) isErrorGlobal = true;
+      }
     }
-    const response = await handleSubmit(e);
-    setResponseData(response);
-    if (responseData && responseData.ok)      
-      return toast({
-        isClosable: true,
-        status: "success",
-        duration: 4000,
-        position: "top",
-        title: t("Thanks for cooperation !"),
-      });
+    if (isErrorGlobal) return;
+    await handleSubmit(e);
+    setData({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+    setErrors({
+      name: false,
+      email: false,
+      subject: false,
+      message: false,
+    });
   };
 
   useEffect(() => {
@@ -67,115 +78,115 @@ function Support() {
 
   return (
     <div className="support">
-      <div class="container">
-        <span class="big-circle"></span>
-        <div class="form">
-          <div class="contact-info">
-            <h3 class="title">{t("Let's get in touch")}</h3>
-            <p class="text">
+      <div className="container">
+        <span className="big-circle"></span>
+        <div className="form">
+          <div className="contact-info">
+            <h3 className="title">{t("Let's get in touch")}</h3>
+            <p className="text">
               {t(
                 "Stay in complete communication to ensure your complete satisfaction"
               )}
             </p>
 
-            <div class="info">
-              <div class="information">
+            <div className="info">
+              <div className="information">
                 <i className="fa-solid fa-map-location"></i>
                 <p>92 Cherry Drive Uniondale, NY 11553</p>
               </div>
-              <div class="information">
+              <div className="information">
                 <i className="fa-regular fa-envelope"></i>
                 <p>support@postbet.ae</p>
               </div>
             </div>
 
-            <div class="social-media">
+            <div className="social-media">
               <p>{t("Connect with us :")}</p>
-              <div class="social-icons">
-                <a href="#">
+              <div className="social-icons">
+                <a
+                  href="https://www.facebook.com/profile.php?id=61567779457730"
+                  target="blank"
+                >
                   <i className="fab fa-facebook-f"></i>
                 </a>
-                <a href="#">
+                <a href="https://x.com/postbett" target="blank">
                   <i className="fab fa-twitter"></i>
                 </a>
-                <a href="#">
+                <a href="https://www.instagram.com/post_bet" target="blank">
                   <i className="fab fa-instagram"></i>
-                </a>
-                <a href="#">
-                  <i className="fab fa-linkedin-in"></i>
                 </a>
               </div>
             </div>
           </div>
 
-          <div class="contact-form">
-            <span class="circle one"></span>
-            <span class="circle two"></span>
+          <div className="contact-form">
+            <span className="circle one"></span>
+            <span className="circle two"></span>
             <form onSubmit={(e) => handleCheckBeforeSubmit(e)}>
-              <h3 class="title">{t("Message Via Email")}</h3>
-              <div class="input-container">
+              <h3 className="title">{t("Message Via Email")}</h3>
+              <div className="input-container">
                 <label for="name">{t("Username")}</label>
                 <input
                   type="text"
                   name="name"
-                  class="input"
+                  className="input"
                   id="name"
                   value={data.name}
                   onChange={(e) => handleChange(e)}
                 />
-                <ValidationError
-                  prefix="Username"
-                  field="name"
-                  errors={state.errors}
-                />
               </div>
-              <div class="input-container">
+              {errors.name && (
+                <p>
+                  {t("Username")} {t("Can’t be Empty")}
+                </p>
+              )}
+              <div className="input-container">
                 <label for="email">{t("Email")}</label>
                 <input
                   type="email"
                   name="email"
-                  class="input"
+                  className="input"
                   id="email"
                   value={data.email}
                   onChange={(e) => handleChange(e)}
                 />
-                <ValidationError
-                  prefix="Email"
-                  field="email"
-                  errors={state.errors}
-                />
               </div>
-              <div class="input-container">
+              {errors.email && (
+                <p>
+                  {t("Email")} {t("Can’t be Empty")}
+                </p>
+              )}
+              <div className="input-container">
                 <label for="subject">{t("Subject")}</label>
                 <input
                   type="text"
                   name="subject"
-                  class="input"
+                  className="input"
                   id="subject"
                   value={data.subject}
                   onChange={(e) => handleChange(e)}
                 />
-                <ValidationError
-                  prefix="Subject"
-                  field="subject"
-                  errors={state.errors}
-                />
               </div>
-              <div class="input-container textarea">
+              {errors.subject && (
+                <p>
+                  {t("Subject")} {t("Can’t be Empty")}
+                </p>
+              )}
+              <div className="input-container textarea">
                 <label for="message">{t("Message")}</label>
                 <textarea
                   name="message"
-                  class="input"
+                  className="input"
                   id="message"
                   value={data.message}
                   onChange={(e) => handleChange(e)}
                 ></textarea>
-                <ValidationError
-                  prefix="Message"
-                  field="message"
-                  errors={state.errors}
-                />
               </div>
+              {errors.message && (
+                <p>
+                  {t("Message")} {t("Can’t be Empty")}
+                </p>
+              )}
               <button type="submit" className="btn" disabled={state.submitting}>
                 {t("Submit")}
               </button>
